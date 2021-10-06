580e3114c8c60b6a39d852e155b519eabfd0a7fd
koprosia <magda.koprowska98@gmail.com>
2021-10-06 11:58:00 +0200

    key listening added

--- snake.py
+++ snake.py
@@ -1,23 +1,38 @@
 from turtle import Turtle, Screen
 
-segments = []
+
 screen = Screen()
 
 
 class Snake:
     def __init__(self):
-        self.segments = segments
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
         for i in range(3):
             new_segment = Turtle(shape="square")
             new_segment.color("white")
             new_segment.penup()
             new_segment.goto(x=0 - i * 20, y=0)
-            segments.append(new_segment)
+            self.segments.append(new_segment)
 
     def move(self):
-        for seg_num in range(len(segments) - 1, 0, -1):
-            segments[seg_num].goto(segments[seg_num - 1].xcor(), segments[seg_num - 1].ycor())
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            self.segments[seg_num].goto(self.segments[seg_num - 1].xcor(), self.segments[seg_num - 1].ycor())
 
-        segments[0].forward(10)
+        self.segments[0].forward(10)
         screen.update()
+
+    def up(self):
+        self.segments[0].setheading(90)
+
+    def down(self):
+        self.segments[0].setheading(270)
+
+    def right(self):
+        self.segments[0].setheading(0)
+
+    def left(self):
+        self.segments[0].setheading(180)
 
