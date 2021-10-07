8feb92231359825139bbd688cf9e28ced26b8509
koprosia <magda.koprowska98@gmail.com>
2021-10-07 11:45:27 +0200

    collision with tail and walls added

--- snake.py
+++ snake.py
@@ -1,5 +1,10 @@
 from turtle import Turtle, Screen
 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 
 screen = Screen()
 
@@ -8,31 +13,42 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for i in range(3):
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(x=0 - i * 20, y=0)
-            self.segments.append(new_segment)
+            self.add_segment(position_x=0-i*20, position_y=0)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             self.segments[seg_num].goto(self.segments[seg_num - 1].xcor(), self.segments[seg_num - 1].ycor())
 
-        self.segments[0].forward(10)
+        self.head.forward(10)
         screen.update()
 
+    def add_segment(self, position_x,position_y):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(x=position_x, y=position_y)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].xcor(),self.segments[-1].ycor() )
+
     def up(self):
-        self.segments[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.segments[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def right(self):
-        self.segments[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
 
     def left(self):
-        self.segments[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
